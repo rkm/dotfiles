1959db0f3534b958ef46bc0f2f152a5ef57cc43d
Ruairidh MacLeod <ruairidh@rkm.dev>
2022-07-22 21:08:55 +0100

    add shellcheck strict mode

--- linux/bin/refresh-ssh-agent.bash
+++ linux/bin/refresh-ssh-agent.bash
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 sock=$(find /tmp/ssh-* -type s -name "agent.*" -print0 | xargs -0 -r ls -1 -t | head -1)
-export SSH_ATUH_SOCK="$sock"
+export SSH_ATUH_SOCK="${sock}"
